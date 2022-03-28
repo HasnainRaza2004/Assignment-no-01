8cb75a59b8b4e80436f46eab05c1974806e336a4
HasnainRaza2004 <hr9520763@gmail.com>
2022-03-28 09:36:07 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// +++++++++++++++++QUESTION NO 01++++++++++++++++++
+
+// Task to convert/return an object keys into a new array
+
+
+// let obj = {
+// name : "Hasnain",
+// password : 97442131
+// };
+
+// let objkeys = Object.keys(obj);
+// console.log(objkeys);
+
+
+// +++++++++++++++++QUESTION NO 02++++++++++++++++++
+
+// Task to convert/return an object values into a new array
+
+// let obj = {
+//     name : "Hasnain",
+//     password : 97442131
+//     };
+    
+//     let objkeys = Object.values(obj);
+//     console.log(objkeys);
